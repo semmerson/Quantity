6df6c5955f89ca8ea9dd19290f94f71aa4a484d0
Steven R. Emmerson <emmerson.steven@gmail.com>
2025-08-16 13:31:09 -0600

    Made hash(), equal_to(), and less() separate headers

---
+++ src/DimensionHash.h
@@ -0,0 +1,44 @@
+/**
+ * This file defines a functional class for hashing a physical dimension.
+ *
+ *        File: DimensionHash.h
+ *  Created on: Aug 13, 2025
+ *      Author: Steven R. Emmerson
+ *
+ * Copyright 2025 Steven R. Emmerson. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Dimension.h"
+
+#include <functional>
+
+namespace std {
+    using namespace quantity;
+
+    /// Function class for hashing a dimension.
+    template<>
+    struct hash<Dimension> {
+        /**
+         * Returns the hash code of a dimension.
+         * @param[in] dim   The dimension
+         * @return          The hash code of the dimension
+         */
+        size_t operator()(const Dimension& dim) const noexcept {
+            return dim.hash();
+        }
+    };
+} // Namespace std
